d9d256247f70176a4af47dfa96c90943455fb4bf
Antonio Rodríguez <antoniorodriguezruiz@gmail.com>
2015-09-27 02:41:36 +0200

    thumbs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 /*jslint node: true */
 'use strict';
 
-var fs = require('fs'),
+var $Q = {},
+  fs = require('fs'),
   express = require('express'),
   multer  = require('multer'),
   _ = require('underscore'),
+  gm = require('gm'),
   storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads');
@@ -38,7 +40,21 @@ var fs = require('fs'),
   upload = multer({ storage: storage }),
   app = express();
 
-
+$Q.utils = {
+  readFileBinary: function (res, file) {
+    fs.readFile(file, function (err, data) {
+      if (err) {
+        console.log(err);
+        console.log(file);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('Error 404');
+      } else {
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.end(data);
+      }
+    });
+  }
+}
 
 app.post('/send-image', upload.single('image'), function (req, res, next) {
   res.redirect('/');
@@ -52,9 +68,27 @@ app.use(express.static('uploads'));
 
 app.get('/images/:size/:image', function (req, res, next) {
   var size = req.param('size'),
-    image = req.param('image');
-  console.log('although this matches');
-  next();
+    image = req.param('image'),
+    file = 'content/thumbs/' + size + '/'+ image;
+  //gm('uploads/' + image).resize(width [, height [, options]])
+  
+  fs.exists(file, function (exists) {
+    if (exists) {
+      $Q.utils.readFileBinary(res, file);
+    } else {
+      gm('uploads/' + image)
+        .resize(size)
+        .autoOrient()
+      .write(file, function (err) {
+        console.log('WRITE: ' + file);
+        $Q.utils.readFileBinary(res, file);
+      });
+    }
+  });
+
+
+  
+  //next();
 });
 
 app.listen(3000);
